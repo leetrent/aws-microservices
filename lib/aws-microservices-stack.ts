2772fd1ca751059c58a986aaa0ceb21ca2a8aa3a
LeeTrent <lee.trent.1@gmail.com>
2022-11-19 10:55:51 -0600

    Added product function

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    console.log('__dirname', __dirname);
 
     /////////////////////////////////////////////////
     // PRODUCT TABLE
@@ -20,5 +24,32 @@ export class AwsMicroservicesStack extends cdk.Stack {
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode: BillingMode.PAY_PER_REQUEST
     });
+
+    //////////////////////////////////////////////////
+    // PRODUCT FUNCTION PROPERTIES
+    //////////////////////////////////////////////////
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ['aws-sdk']
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_16_X
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // PRODUCT FUNCTION
+    ///////////////////////////////////////////////////////////////////////////
+    const productFuntion = new NodejsFunction(this, 'productLambdaFunction', {
+      entry: join(__dirname, '/../src/product/index.js'),
+      ...nodeJsFunctionProps,
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    // GRANT READ-WRITE PRIVELEGES TO PRODUCT FUNCTION
+    ///////////////////////////////////////////////////////////////////////////
+    productTable.grantReadWriteData(productFuntion);
   }
 }
