85f45dc58b6e8e9ba883ff2a3881cc3b9796a029
LeeTrent <lee.trent.1@gmail.com>
2022-12-13 07:07:12 -0600

    Completed prep for basket microservices development

--- src/basket/index.js
+++ src/basket/index.js
@@ -1,3 +1,15 @@
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Links:
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/index.html#aws-sdkclient-dynamodb
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_util_dynamodb.html
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/welcome.html
+// Notes:
+// npm install @aws-sdk/client-dynamodb
+// npm install @aws-sdk/util-dynamodb
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 exports.handler = async function(event) {
     console.log("[basketMicroservice] => (event):", event);
     console.log("[basketMicroservice] => (request):", JSON.stringify( event, undefined, 2));
