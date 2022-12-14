f6c9469c65f6ac996cb951646172645075908e2f
LeeTrent <lee.trent.1@gmail.com>
2022-12-13 20:42:05 -0600

    Staring basket microservice development

--- src/basket/index.js
+++ src/basket/index.js
@@ -16,9 +16,46 @@ exports.handler = async function(event) {
     console.log("[basketMicroservice] => (event.httpMethod):", event.httpMethod);
     console.log("[basketMicroservice] => (event.pathParameters):", event.pathParameters);
 
-    return {
-        statusCode: 200,
-        headers: {"Content-Type": "text/plain"},
-        body: `[basketMicroservice] => (event.path): ${event.path}\n`
-    };
+    let body = null;
+    try {
+        switch (event.httpMethod) {
+            case "GET":
+                if (event.pathParameters != null) {
+                    body = await getBasket(event.pathParameters.userName);
+                } else {
+                    body = await getAllBaskets();
+                }
+                break;
+            case "POST":
+                if (event.path == "/basket/checkout") {
+                    body = await checkoutBasket(event);
+                } else {
+                    body = await createBasket(event);
+                }                
+                break;
+            case "DELETE":
+                body = await deleteBasket(event.pathParameters.userName);
+                break;
+            default:
+                throw new Error(`Unsupported route: ${event.httpMethod}`);
+        }  
+        console.log("[basketMicroservice] => (body):", body);  
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Successfully completed ${event.httpMethod} operaton.`,
+                body: body
+            })
+        }   
+    } catch (exc) {
+        console.log("[basketMicroservice] => (exception):", exc);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Failed to perform operation in basket microservice",
+                errorMessage: exc.message,
+                errorStack: event.stack
+            })
+        };
+    }
 }
