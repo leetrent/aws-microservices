845f7d79b7918c1f986bcd8d8ddfd517f6abea13
LeeTrent <lee.trent.1@gmail.com>
2023-01-03 08:16:47 -0600

    Developing order microservice

--- src/order/index.js
+++ src/order/index.js
@@ -1,8 +1,45 @@
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { ddbClient } from "./ddbClient";
+
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain"},
-        body: `[order microservice] => (event.path): ${event.path}\n`
-    };
+
+    const eventType = event["detail-type"];
+    if (eventType !== undefined) {
+        await eventBridgeInvocation(event);
+    } else {
+        return await ApiGatewayInvocation(event);
+    }
+}
+
+const eventBridgeInvocation = async(event) => {
+    await createOrder(event.detail)
+}
+
+const ApiGatewayInvocation = async(event) => {
+
+}
+
+const createOrder = async(backetCheckoutEvent) => {
+    const logSnippet = "[orderMicroservice][createOrder] =>";
+    try {
+        const orderDate = new Date().toISOString();
+        backetCheckoutEvent.orderDate = orderDate; 
+        console.log(`${logSnippet} (backetCheckoutEvent): ${backetCheckoutEvent}`);
+    
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(backetCheckoutEvent || {})
+        };
+    
+        const createResult = await ddbClient.send(new PutItemCommand(params));
+        console.log(`${logSnippet} (createResult): ${createResult}`);
+    
+        return createResult;
+
+    } catch (exc) {
+        console.log(`${logSnippet} (Exception): ${exc}`);
+        throw exc;
+    }
 }
